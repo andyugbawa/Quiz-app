25ecc084cdc7090cd8c059e2affd0383b43ed601
andy <andyugbawa@gmail.com>
2024-05-18 12:21:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("HELLO")
+
+const questions =[
+    {
+        question: 'How many bits make  a byte',
+        answers:[
+            {text:"16 bits", correct:false},
+            {text:"8 bits", correct:true},
+            {text:"24 bits", correct:false},
+            {text:"12 bits", correct:false}
+        ]
+    },
+
+    {
+        question: 'The first search engine on the internet',
+        answers:[
+            {text:"Archie", correct:true},
+            {text:"Google", correct:false},
+            {text:"Bing", correct:false},
+            {text:"Yahoo", correct:false}
+        ]
+    },
+
+    {
+        question: 'The number of bits used by IPv6 address is',
+        answers:[
+            {text:"16", correct:false},
+            {text:"32", correct:false},
+            {text:"64", correct:false},
+            {text:"128", correct:true}
+        ]
+    },
+
+
+    {
+        question: 'The first web browser invented in 1990 was',
+        answers:[
+            {text:"WorldWideWeb", correct:true},
+            {text:"internet Explorer", correct:false},
+            {text:"Mosiac", correct:false},
+            {text:"Nexus", correct:true}
+        ]
+    },
+]
+
+const question
